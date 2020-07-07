0f8fef0779f58253e4e8e832d839f356b0840de4
Devon Layton <dml4h@mtmail.mtsu.edu>
2020-07-07 15:39:55 -0500

    added nav-bar to app.js

--- social-app/src/App.js
+++ social-app/src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header"> */}
+      <nav class="shadow-sm navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+      <span class="navbar-brand">Logo</span>
+        <ul class="navbar-nav">
+          <li class="navbar-item">
+          <a class="nav-link" href="./">thing1</a>
+          </li>
+          <li class="navbar-item">
+            <a class="nav-link" href="./">thing2</a>
+          </li>{" "}
+        </ul>
+      </nav>
+
+      <div class="container-fluid">
+        <div class="shadow p-3  mb-4 bg-dark justify-content-center text-white row row-cols-1">
+          Header
+        </div>
+        <div class="row row-cols-3">
+          <div class="col bg-primary">Column1</div>
+          <div class="col bg-secondary">Column2</div>
+          <div class="col bg-info">Column2</div>
+        </div>
+      </div>
+      {/* </header> */}
     </div>
   );
 }
